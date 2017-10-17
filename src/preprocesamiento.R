8ae4c8972977bb8a7121c1f29665e8ad30620060
castudil <cesar.astudillo@gmail.com>
2017-10-17 18:44:50 -0300

    actualizacion al preprocesamiento

--- src/preprocesamiento.R
+++ src/preprocesamiento.R
@@ -2,19 +2,31 @@
 
 # Cargado de datos desde el directorio local... 
 library(readr)
-OriginalData <- read_delim("C:/Users/Lenovoo/Dropbox/Alejandro Cataldo/git project/data/data.csv",";", escape_double = FALSE, trim_ws = TRUE) 
+#OriginalData <- read_delim("C:/Users/Lenovoo/Dropbox/Alejandro Cataldo/git project/data/data.csv",";", escape_double = FALSE, trim_ws = TRUE) 
+OriginalData <- read_csv("../data/data.csv")
 
-# Creaci�n de nueva base de datos (binaryData) sin las columnas no-numericas.
+#names(OriginalData)
+#str(OriginalData)
+#dim(OriginalData)
+# 8084   26
+
+# Creaci?n de nueva base de datos (binaryData) sin las columnas no-numericas.
 binaryData <- OriginalData[,-c(1,2,3,4)] 
 
-# Identificaci�n y almacenamiento de las instancias que poseen ceros en todos los campos
+#remover instancias que contienen solo ceros
+# Identificación y almacenamiento de las instancias que poseen ceros en todos los campos
 indremoved = which(apply(binaryData, 1, function(x) all(x == 0)) )
+paste("se removeran ",length(indremoved)," instancias que contienen solo ceros")
+#no parece haber un patron en las encuestas con ceros
 
-#Removimientolas de instancias que poseen ceros en todos los campos de ambas bases de datos
+#Removimiendo instancias que poseen ceros en todos los campos de ambas bases de datos
 binaryData = binaryData[ -indremoved, ]
 OriginalData = OriginalData[ -indremoved, ]
 
-#No existen datos faltantes!
+#dim(binaryData)
+#[1] 7861   22
+
+#No existen datos faltantes! :)
 
 #Calculo de componentes principales
 pc <- prcomp(binaryData)
@@ -22,26 +34,32 @@ plot(pc)
 plot(pc, type='l')
 summary(pc) 
 
-#Con el fin de representar al menos el 85% de la informaci�n de los datos, se trabaja sobre la base de los 12 primeros componentes principales
+#Con el fin de representar al menos el 85% de la informaci?n áéíóú de los datos, se trabaja sobre la base de los 12 primeros componentes principales
 comp <- data.frame(pc$x[,1:12])
-plot(comp, pch=16, col=rgb(0,0,0,0.5))
+plot(comp[,1:3], pch=16, col=rgb(0,0,0,0.5))
 
 
 # Apply k-means with k=3
 
-install.packages("cluster")
+#install.packages("cluster")
 library(cluster)
-install.packages("fpc")
+#install.packages("fpc")
 library(fpc)
 
 #KMEANS K = 3
-k <- kmeans(comp, 3, nstart=25, iter.max=1000)     
-
+k<-9
+model <- kmeans(binaryData, k, nstart=25, iter.max=1000)     
 #Calulo de silhouette Para cada cluster formado
 dissE <- daisy(comp) 
 dE2   <- dissE^2
-sk2   <- silhouette(k$cl, dE2)
+sk2   <- silhouette(model$cl, dE2)
+paste("k: ",k," silhouette: ",mean(sk2[,3]),"")
+
+silhouette_kmeans<-c(0,0.372096819507349,0.293974180144305,0.274455229227329,0.249989801452009,0.246038468119303,0.252897798753004,0.244953331962789,0.247261680502461)
+plot(silhouette_kmeans)
+
 plot(sk2, col = c("red", "green", "blue","black"))
+
 
 plotcluster(comp, k$cluster)
 clusplot(comp, k$cluster, color=TRUE, shade=TRUE, 
